aa6a06b7f4a6d309d4999cf2260e43f9eabefc8c
-v <-v>
2024-04-09 21:30:17 +0000

    update to ES6 imports. Event handlers don't work yet

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const Home = () => <div>I'm the very best home component</div>;
+const Home = () => (
+  <div>
+    <div>I'm the very best home component</div>
+    <button onClick={() => console.log("Hi there!")}>Press me</button>
+  </div>
+);
 
 export default Home;
